b2ba3648231fb4cb19d9f61fa4487646dffa5aa3
azaatri <amine.zaatri@gmail.com>
2018-12-15 17:32:39 +0100

    webpack encore init

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,18 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
+// require jQuery normally
+
+const $ = require('jquery');
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+require('bootstrap/dist/css/bootstrap.min.css');
+// any CSS you require will output into a single css file (app.css in this case)
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
---
+++ assets/js/datatables.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+// require jQuery normally
+
+require('./app.js');
+require('datatables.net-bs4');
+require('datatables.net-fixedheader-bs4');
+require('datatables.net-responsive-bs4');
+require('datatables.net-bs4/css/dataTables.bootstrap4.min.css');
+require('datatables.net-fixedheader-bs4/css/fixedHeader.bootstrap4.min.css');
+require('datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css');
